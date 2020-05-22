e344dd688e1aec594fd208389984aed01a8c3d67
Robert Grant <robertstats@live.com>
2020-05-22 19:03:14 +0100

    v1.1: trimmed away loose ends

--- timelogger.cpp
+++ timelogger.cpp
@@ -8,23 +8,16 @@ using std::endl;
 using std::time_t;
 using std::ofstream;
 using std::ios;
-using std::stringstream;
+using std::cout;
 using std::string;
-using std::to_string; // this will require -std=c++11 when compiling
-using std::chrono::system_clock;
-using std::ctime;
 
 int main() {
-	stringstream sstrm;
 	string stime;
 	char chtime[100];
 	ofstream logcsv;
-	logcsv.open("log.csv", ios::out | ios::app);
+	logcsv.open("/Users/robert/git/timelogger/log.csv", ios::out | ios::app);
 	std::time_t tm = std::time(nullptr);
-  //auto chronotm = system_clock::now();
-  //time_t tttm = system_clock::to_time_t(chronotm);
 	std::strftime(chtime, sizeof(chtime), "%T %a %d/%m/%Y", std::localtime(&tm));
-	//stime = to_string(ctime(&tttm));
 	logcsv << chtime << endl;
   logcsv.close();
   return 0;
